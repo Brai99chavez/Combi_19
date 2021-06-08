3d76cfac7e22897cf4d4ce2f99cdbec4788b96fe
Tonyraz18 <anthony.infor18@gmail.com>
2021-06-08 16:59:16 -0300

    cambios

--- resources/views/admin/membresias/homeMembresia.blade.php
+++ resources/views/admin/membresias/homeMembresia.blade.php
@@ -5,9 +5,9 @@
 <div class="formulary">
     @foreach($membresias as $membresia)
             <strong>Nombre:</strong><br>
-            {{$membresia->nombre}} <br>
+            {{$membresia->nombre}}<br>
             <strong>Descuento:</strong><br>
-            {{$membresia->descuento}} <br>
+            {{$membresia->descuento}}%<br>
             <form action="{{route('updatemembresias')}}" method="POST">
                 @csrf
                 <input type="hidden" name="id_membresia"value="{{$membresia->id_membresia}}">
