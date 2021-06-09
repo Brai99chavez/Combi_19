b14a1947a25bde30b81e042a2fd0af06eba063f1
Brai_UwU <zardark001.bc@gmail.com>
2021-06-09 18:29:44 -0300

    arreglos en los seeders

--- database/seeders/viajesSeeder.php
+++ database/seeders/viajesSeeder.php
@@ -24,5 +24,23 @@ class viajesSeeder extends Seeder
     $viaje1->hora = Carbon::createFromTime('15','30','00');
     $viaje1->save();
 
+    $viaje2 = new Viajes();
+    $viaje2->id_chofer = 1;
+    $viaje2->id_combi = 2;
+    $viaje2->id_ruta = 2;
+    $viaje2->precio = 10000;
+    $viaje2->fecha = Carbon::createFromFormat('d/m/Y', '5/8/2021');
+    $viaje2->hora = Carbon::createFromTime('15','30','00');
+    $viaje2->save();
+
+    $viaje3 = new Viajes();
+    $viaje3->id_chofer = 4;
+    $viaje3->id_combi = 3;
+    $viaje3->id_ruta =3;
+    $viaje3->precio = 10000;
+    $viaje3->fecha = Carbon::createFromFormat('d/m/Y', '9/7/2021');
+    $viaje3->hora = Carbon::createFromTime('15','30','00');
+    $viaje3->save();
+
     }
 }
