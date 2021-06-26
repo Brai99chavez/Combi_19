c4f17fa2eda1b7cdaf2eae6f46fa16fec8f4e8f2
Tonyraz18 <anthony.infor18@gmail.com>
2021-06-26 19:56:08 -0300

    request de la tarjeta

---
+++ app/Http/Requests/clienteConTarjetaRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class empleadosRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nombre'=>'required|max:40',
+            'apellido'=>'required|max:40',
+            'dni'=>'required|max:10|unique:Usuarios',
+            'email'=>'required|email|unique:Usuarios',
+            'contraseña'=>'required',
+            'contraseñavalidation' => 'required|same:contraseña',
+            'tarjeta' => 'required|max:16',
+            'codigo' => 'required|max:3',
+            'fechaVenc' => 'required'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'dni.unique' => 'El DNI ingresado ya pertenece a un empleado registrado',
+            'email.unique' => 'El email ingresado ya pertenece a un empleado registrado',
+            'contraseñavalidation.same' => 'Las contraseñas no coinciden',
+            'nombre.max' => 'El nombre debe tener menos de 40 caracteres',
+            'apellido.max' => 'El apellido debe tener menos de 40 caracteres',
+            'dni.max' => 'DNI invalido',
+            'required' => 'Los campos no pueden estar vacios',
+            'tarjeta.max' => 'La tarjeta ingresada es invalida',
+            'codigo.max' => 'Codigo de seguridad invalido',
+        ];
+    }
+}
