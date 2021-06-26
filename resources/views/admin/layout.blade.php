4a23af0cf72b59d625eb3af408b6fbb697099caa
Brai_UwU <zardark001.bc@gmail.com>
2021-06-26 19:22:49 -0300

    cambios css

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -2,10 +2,6 @@
 
 @section('navLinks')
 <a href="{{route('homeadmin')}}">Home</a>
-<a href="{{route('homeviajes')}}">Viajes</a>
-<a href="{{route('homeciudades')}}">Ciudades</a>
-<a href="{{route('homeinsumos')}}">Insumos</a>
-<a href="{{route('homemembresias')}}">Membresias</a>
-<a href="{{route('homecombis')}}">Combis</a>
-<a href="{{route('homeEmp')}}">Empleados</a>
+<a href="{{route('reports')}}">Reports</a>
+
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,30 +7,52 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="../resources/css/style.css">
     <link rel="stylesheet" href="../resources/css/navbar.css">
-    <link rel="stylesheet" href="../resources/css/formulary.css">
-    <link rel="stylesheet" href="../resources/css/table.css">
+    <link rel="stylesheet" href="../resources/css/formularys.css">
+    <script src="../resources/js/navbar.js" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" integrity="sha256-46r060N2LrChLLb5zowXQ72/iKKNiw/lAmygmHExk/o=" crossorigin="anonymous" />
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script src="https://kit.fontawesome.com/0172a1fd00.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     
     @yield('head')
 </head>
     <body>
-        <!-- HEADER ENCABEZADO-->
-        <header class="header">
-            <div class="logo">
-                <img src="../resources/imagenes/combi_19_logo.png" alt="">
-                <h1>@yield('headerTitle')
-                </h1>
-            </div>
-            <nav class="menu">
+        <nav>
+            <div class="logo"><img src="../resources/imagenes/logo_nav.png" alt=""></div>
+            <div class="openMenu"><i class="fa fa-bars"></i></div>
+            <ul class="mainMenu">
                 @yield('navLinks')
-                <a href="{{route('logOut')}}"> CERRAR SESION</a>
-            </nav>
-        </header>
-        <br>
-            @yield('content')
-            @yield('js')
-    </body>
+                <li><a href="{{route('logOut')}}"> Cerrar Sesion</a></li>
+                <div class="closeMenu"><i class="fa fa-times"></i></div>
+                <span class="icons">
+                    <i class="fab fa-facebook"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-twitter"></i>
     
+                </span>
+            </ul>
+        </nav>
+        <div class="telefono">
+            <p>Telefono: (0221)153141464</p>
+        </div>
+        <main>
+                @yield('content')
+        </main>
+        
+        <div class="footer">
+            <div class="redes">
+                <i class="fab fa-facebook" ></i>
+                <p>Facebook</p>
+            </div>
+            <div class="redes">
+                <i class="fab fa-instagram"></i>
+                <p>Instagram</p>
+            </div>
+            <div class="redes">
+                <i class="fab fa-twitter"></i>
+                <p>Twitter</p>
+            </div>
+        </div>
+        @yield('js')
+    </body>
 </html>
