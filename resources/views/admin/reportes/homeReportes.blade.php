a5c0c1cf434eba741f6af8426302650b495a1749
Brai_UwU <zardark001.bc@gmail.com>
2021-07-01 04:42:32 -0300

    asdasd

--- resources/views/admin/reportes/homeReportes.blade.php
+++ resources/views/admin/reportes/homeReportes.blade.php
@@ -5,7 +5,7 @@
     <div class="formulary">
         <h2>Reportes</h2>
 
-            <button type="submit" onclick=location.href="{{route('ingresoIDViaje')}}">Pasajeros de un Viaje</button><br><br><br>
+            <button type="submit" onclick=location.href="{{route('ingresoIDViaje')}}">Pasajeros de Viaje</button><br><br><br>
             <button type="submit" onclick=location.href="{{route('ingresoPeriodo')}}"> Viajes en un periodo</button><br><br><br>
             <button type="submit" onclick=location.href="{{route('reportesPasajerosCOVID')}}">Pasajeros con sintomas COVID</button><br><br><br>
     </div>
