b54d706bdb18e09f2c0fce0e890ac2a6b78c9580
Brai_UwU <zardark001.bc@gmail.com>
2021-07-02 11:30:01 -0300

    ULTIMO LASTO FINAL ULTIMATE PUSH

--- resources/views/admin/reportes/reportePasajerosConSintomasCOVID.blade.php
+++ resources/views/admin/reportes/reportePasajerosConSintomasCOVID.blade.php
@@ -3,26 +3,28 @@
 @section('headerTitle','Reporte Pasajeros Con Sintomas COVID')
 @section('content')
 <div class="formulary">
+    <h2>Pasajeros con Covid</h2>
     @if($pasajeros->isNotEmpty())
-        @foreach($pasajeros as $pasajero)
-            <strong>Nombre:</strong>{{$pasajero->nombre}}<br>
-            <strong>Apellido: </strong>{{$pasajero->apellido}}<br>
-            <strong>DNI: </strong>{{$pasajero->dni}}<br>
-            <strong>Email: </strong>{{$pasajero->email}}<br>
-            <ul>
-                <strong>Sintomas</strong>
-                @foreach($sintomas as $sintoma)
-                    @if($sintoma->id_usuario == $pasajero->id_usuario)
-                    <li>{{$sintoma->nombre_sintoma}}</li>
-                    @endif
-                @endforeach
-            </ul>
-            <p>-------------------------------------</p>
+    <form action="">
+    @foreach($pasajeros as $pasajero)
+    <strong>Nombre:</strong>{{$pasajero->nombre}}<br>
+    <strong>Apellido: </strong>{{$pasajero->apellido}}<br>
+    <strong>DNI: </strong>{{$pasajero->dni}}<br>
+    <strong>Email: </strong>{{$pasajero->email}}<br>
+    <ul style="list-style: none">
+        <strong>Sintomas</strong>
+        @foreach($sintomas as $sintoma)
+            @if($sintoma->id_usuario == $pasajero->id_usuario)
+            <li>{{$sintoma->nombre_sintoma}}</li>
+            @endif
         @endforeach
+    </ul>
+    <p>-----------------------------------------------------------------</p>
+@endforeach</form>
     @else
     <h2>No se registran pasajeros con sintomas COVID</h2>
     <br>
-    <a href="{{route('Home_Reportes')}}"><button>Volver a reportes</button></a>
+    <button onclick=location.href="{{route('Home_Reportes')}}" >Volver a reportes</button>
     @endif
 </div>
 @endsection
